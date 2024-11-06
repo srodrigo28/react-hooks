62474918223185c2ce035796180090f1de8e9d30
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-11-06 19:25:37 -0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { useState,useEffect } from "react"
+
 export function App(){
+  const [entity, setEntity] = useState("users");
+
+  useEffect( () => {
+    const fetchResourceType = async () => {
+      const response = await fetch(
+        `http://localhost:3000/${entity}`
+      );
+      const responseJson = await response.json();
+
+      console.log(responseJson)
+    };
+
+    fetchResourceType();
+  }, [entity])
+
+  const changeEntity = (entity) => {
+    setEntity(entity);
+    console.log(entity);
+  }
   return <div>
     <h1>Hello React Hooks</h1>
+    <p>Entity: {entity}</p>
+
+    <div style={{ display: "flex", alignItems: "center" }}>
+       <button onChange={ () => changeEntity("users")}>Usuários</button>
+       <button onChange={ () => changeEntity("customers")}>Clientes</button>
+       <button onChange={ () => changeEntity("products")}>Produtos</button>
+    </div>
   </div>
 }
